414fbaf0b6a44c190430166a844b4f1e28709c9f
Tung Nguyen Viet <tungto2010@gmail.com>
2019-12-17 10:09:55 +0700

    Quiz

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Tung Nguyen on 9/20/19.
+//  Copyright © 2019 Tung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+    @IBOutlet var quesntionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func showNextAnswer(_ sender: UIButton){
+        
+    }
+}
+
